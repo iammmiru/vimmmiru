f2dd8b2ef0290a377c127689a04a9003ab8f4840
Miru Lee <32926337+iammmiru@users.noreply.github.com>
2024-06-14 17:37:45 +0200

    Add cool cmp keybindings for luasnip

--- lua/nvim_miru/plugins/auto_completion.lua
+++ lua/nvim_miru/plugins/auto_completion.lua
@@ -7,10 +7,6 @@ return {
 			{ 'hrsh7th/cmp-path' },
 			{ 'saadparwaiz1/cmp_luasnip' },
 			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
-			{ 'hrsh7th/cmp-nvim-lsp-signature-help' },
-			{ 'hrsh7th/cmp-vsnip' },
-			{ 'hrsh7th/vim-vsnip' },
 		},
 		event = 'InsertEnter',
 		config = function()
@@ -40,10 +36,33 @@ return {
 					{ name = "buffer" },
 					{ name = "nvim_lsp" },
 					{ name = "path" },
-					{ name = "vsnip" },
 					{ name = "luasnip" },
-				}
+				},
+
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
 			})
+
+			local ls = require('luasnip')
+			ls.config.set_config({
+				history = false,
+				updateevents = "TextChanged, TextChangedI",
+			})
+
+			vim.keymap.set({ "i", "s" }, "<C-k>", function()
+				if ls.expand_or_jumpable() then
+					ls.expand_or_jump()
+				end
+			end, { silent = true })
+
+			vim.keymap.set({ "i", "s" }, "<C-j>", function()
+				if ls.jumpable(-1) then
+					ls.jump(-1)
+				end
+			end, { silent = true })
 		end,
 	}
 }
