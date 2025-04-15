d99a5cdd31a1d1dbb27bcbc3526d69bc444205fa
Miru Lee <32926337+iammmiru@users.noreply.github.com>
2025-04-15 14:41:13 +0200

    Remove rust.lua in ftplugin
    it seems on_attach gets called later ftplugin

--- lua/nvim_miru/plugins/rust.lua
+++ lua/nvim_miru/plugins/rust.lua
@@ -6,10 +6,66 @@ return {
 		lazy = false, -- This plugin is already lazy
 		init = function()
 			vim.g.rustaceanvim = {
+				tools = {
+					float_win_config = {
+						border = "rounded", max_width = 120,
+					},
+				},
 				server = {
 					capabilities = require('cmp_nvim_lsp').default_capabilities(),
-					on_attach = function(client, buffer)
-						lsp_attach.lsp_attach(client, buffer)
+					on_attach = function(client, bufnr)
+						lsp_attach.lsp_attach(client, bufnr)
+
+						vim.keymap.set(
+							"n",
+							"<leader>ca",
+							function()
+								vim.cmd.RustLsp('codeAction')
+							end,
+							{ silent = true, buffer = bufnr }
+						)
+						vim.keymap.set(
+							"n",
+							"<leader>ce",
+							function()
+								vim.cmd.RustLsp('explainError', 'cycle')
+							end,
+							{ silent = true, buffer = bufnr }
+						)
+						vim.keymap.set(
+							"n",
+							"K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+							function()
+								vim.cmd.RustLsp({ 'hover', 'actions' })
+							end,
+							{ silent = true, buffer = bufnr }
+						)
+						vim.keymap.set(
+							"n",
+							"<leader>rt",
+							function()
+								vim.cmd.RustLsp({ 'run' })
+							end,
+							{ silent = true, buffer = bufnr }
+						)
+						vim.keymap.set(
+							"n",
+							"<leader>ra",
+							function()
+								vim.cmd.RustLsp({ 'runnables' })
+							end,
+							{ silent = true, buffer = bufnr }
+						)
+
+						for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+							local default_diagnostic_handler = vim.lsp.handlers[method]
+							vim.lsp.handlers[method] = function(err, result, context, config)
+								if err ~= nil and err.code == -32802 then
+									return
+								end
+								return default_diagnostic_handler(err, result, context, config)
+							end
+						end
 					end,
 					default_settings = {
 						["rust-analyzer"] = {
