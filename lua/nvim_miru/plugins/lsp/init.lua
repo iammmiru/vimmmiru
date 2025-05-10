cfc831842253f3931a8918d83d578ac65a21b002
Your Name <32926337+iammmiru@users.noreply.github.com>
2025-05-10 09:08:13 +0200

    Adapt for breaking changes from mason-lspconfig

--- lua/nvim_miru/plugins/lsp/init.lua
+++ lua/nvim_miru/plugins/lsp/init.lua
@@ -3,8 +3,8 @@ return {
 		'neovim/nvim-lspconfig',
 		dependencies = {
 			-- LSP Support
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
+			{ 'mason-org/mason.nvim' },
+			{ 'mason-org/mason-lspconfig.nvim' },
 		},
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
@@ -22,10 +22,9 @@ return {
 			vim.api.nvim_create_autocmd('LspAttach', {
 				desc = "LSP actions",
 				callback = function(event)
-				lsp_attach(event.client, event.buf)
+					lsp_attach(event.client, event.buf)
 				end
 			})
-
 
 			require('mason').setup({})
 			require('mason-lspconfig').setup({
@@ -37,90 +36,64 @@ return {
 					'yamlls',
 					'ruff',
 				},
-				handlers = {
-					function(server_name)
-						lspconfig[server_name].setup({})
-					end,
-
-					rust_analyzer = function() end,
-
-					lua_ls = function()
-						lspconfig.lua_ls.setup({
-							settings = {
-								Lua = {
-									diagnostics = {
-										globals = { 'vim' }
-									}
-								}
-							}
-						})
-					end,
-
-					ruff = function()
-						lspconfig.ruff.setup({
-							init_options = {
-								settings = {
-									args = {},
-								}
-							},
-							on_attach = function(client)
-								if client.name == 'ruff' then
-									-- Disable hover in favor of Pyright
-									client.server_capabilities.hoverProvider = false
-								end
-							end
-
-						})
-					end,
-
-					pyright = function()
-						lspconfig.pyright.setup({
-							settings = {
-								pyright = {
-									-- Using Ruff's import organizer
-									disableOrganizeImports = true,
-								},
-								python = {
-									analysis = {
-										-- Ignore all files for analysis to exclusively use Ruff for linting
-										ignore = { '*' },
-									},
-								},
-							},
-
-						})
-					end,
-
-					yamlls = function()
-						lspconfig.yamlls.setup({
-							capabilities = {
-								textDocument = {
-									foldingRange = {
-										dynamicRegistration = false,
-										lineFoldingOnly = true,
-									},
-								},
-							},
-							settings = {
-								yaml = {
-									format = {
-										enable = true
-									},
-									schemaStore = {
-										enable = true
-									},
-									hover = true,
-									validate = true,
-								}
-							}
-
-						})
-					end
-				},
 			})
 
 			vim.diagnostic.config({
 				virtual_text = false,
+			})
+
+			vim.lsp.config('lua_ls', require("nvim_miru.plugins.lsp.lua_ls"))
+
+			vim.lsp.config("ruff", {
+				init_options = {
+					settings = {
+						args = {},
+					}
+				},
+				on_attach = function(client)
+					if client.name == 'ruff' then
+						-- Disable hover in favor of Pyright
+						client.server_capabilities.hoverProvider = false
+					end
+				end
+			})
+
+			vim.lsp.config("pyright", {
+				settings = {
+					pyright = {
+						-- Using Ruff's import organizer
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							-- Ignore all files for analysis to exclusively use Ruff for linting
+							ignore = { '*' },
+						},
+					},
+				},
+
+			})
+			vim.lsp.config("yamlls", {
+				capabilities = {
+					textDocument = {
+						foldingRange = {
+							dynamicRegistration = false,
+							lineFoldingOnly = true,
+						},
+					},
+				},
+				settings = {
+					yaml = {
+						format = {
+							enable = true
+						},
+						schemaStore = {
+							enable = true
+						},
+						hover = true,
+						validate = true,
+					}
+				}
 			})
 
 			require('nvim_miru.plugins.lsp.jdtls')
--- lua/nvim_miru/plugins/mini.lua
+++ lua/nvim_miru/plugins/mini.lua
@@ -16,7 +16,6 @@ return {
 			-- - sd'   - [S]urround [D]elete [']quotes
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require('mini.surround').setup()
-			require('mini.diff').setup()
 
 			-- ... and there is more!
 			--  Check out: https://github.com/echasnovski/mini.nvim
