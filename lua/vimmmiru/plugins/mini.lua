1733abc7597b98a8a7881611f72f0c4ec07d2f95
Miru Lee <32926337+iammmiru@users.noreply.github.com>
2025-05-14 22:22:26 +0200

    feat: replace gitsigns with mini.diff
    - Deleted gitsigns.nvim
    - Added mini.diff

--- lua/vimmmiru/plugins/mini.lua
+++ lua/vimmmiru/plugins/mini.lua
@@ -17,8 +17,18 @@ return {
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require('mini.surround').setup()
 
-			-- ... and there is more!
-			--  Check out: https://github.com/echasnovski/mini.nvim
+			require('mini.diff').setup({
+				view = {
+					style = 'sign',
+					signs = {
+						add = '+',
+						change = '~',
+						delete = '_',
+						topdelete = '‾',
+						changedelete = '~',
+					}
+				}
+			})
 		end,
 	},
 }
