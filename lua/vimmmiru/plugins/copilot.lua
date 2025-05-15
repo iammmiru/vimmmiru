69037e0ad070f97abfa45174612bee87992aa9fa
Miru Lee <32926337+iammmiru@users.noreply.github.com>
2025-05-15 15:43:23 +0200

    feat(copilot): prevent attaching in excluded directories
    - Added `should_attach` function to skip Copilot setup if the current root directory matches any in `exclude_dirs`.

--- lua/vimmmiru/plugins/copilot.lua
+++ lua/vimmmiru/plugins/copilot.lua
@@ -9,7 +9,18 @@ return {
 	cmd = "Copilot",
 	event = "InsertEnter",
 	config = function()
+		local exclude_dirs = { vim.fn.expand("$HOME/aivis/engine-v2") }
 		require("copilot").setup({
+			should_attach = function (_, bufname)
+				vim.print(bufname)
+				local root_dir = require("lspconfig.util").root_pattern(".git")(vim.fn.expand("%:p"))
+				for _, dir in ipairs(exclude_dirs) do
+					if dir == root_dir then
+						return false
+					end
+				end
+				return true
+			end,
 			suggestion = { enabled = false },
 			panel = { enabled = false },
 		})
