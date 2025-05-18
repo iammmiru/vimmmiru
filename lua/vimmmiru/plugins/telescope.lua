9bac03ce4fc59d28d89ea2036bb410d5bd15753a
Miru Lee <32926337+iammmiru@users.noreply.github.com>
2025-05-18 10:50:31 +0200

    feat(telescope): improve keymap descriptions and add <leader>sk to show keymaps
    - Added "Telescope: " prefix to keymap descriptions for better clarity.
    - Added a keymap `<leader>sk` to show Telescope keymaps, with description "Telescope: [S]how [K]eymaps".
    
    wip

--- lua/vimmmiru/plugins/telescope.lua
+++ lua/vimmmiru/plugins/telescope.lua
@@ -3,20 +3,28 @@ return {
 		'nvim-telescope/telescope.nvim',
 		keys = function()
 			vim.keymap.set('n', '<leader>/', require('telescope.builtin').oldfiles,
-				{ desc = '[?] Find recently opened files' })
+				{ desc = 'Telescope: [/] Find recently opened files' })
 			vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers,
-				{ desc = '[ ] Find existing buffers' })
+				{ desc = 'Telescope: [ ] Find existing buffers' })
 
-			vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-			vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+			vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files,
+				{ desc = 'Telescope: [S]earch [F]iles' })
+			vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags,
+				{ desc = 'Telescope: [S]earch [H]elp' })
 			vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
-				{ desc = '[S]earch current [W]ord' })
-			vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+				{ desc = 'Telescope: [S]earch current [W]ord' })
+			vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,
+				{ desc = 'Telescope: [S]earch by [G]rep' })
 			vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics,
-				{ desc = '[S]earch [D]iagnostics' })
-			vim.keymap.set('n', '<leader>gb', require('telescope.builtin').git_branches, { desc = '[G]it [B]ranches'})
-			vim.keymap.set('n', '<leader>gc', require('telescope.builtin').git_commits, { desc = '[G]it [C]ommits'})
-			vim.keymap.set('n', '<leader>gs', require('telescope.builtin').git_status, { desc = '[G]it [S]tatus'})
+				{ desc = 'Telescope: [S]earch [D]iagnostics' })
+			vim.keymap.set('n', '<leader>gb', require('telescope.builtin').git_branches,
+				{ desc = 'Telescope: [G]it [B]ranches' })
+			vim.keymap.set('n', '<leader>gc', require('telescope.builtin').git_commits,
+				{ desc = 'Telescope: [G]it [C]ommits' })
+			vim.keymap.set('n', '<leader>gs', require('telescope.builtin').git_status,
+				{ desc = 'Telescope: [G]it [S]tatus' })
+			vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps,
+				{ desc = 'Telescope: [S]how [K]eymaps' })
 		end,
 		opts = {
 			defaults = {
