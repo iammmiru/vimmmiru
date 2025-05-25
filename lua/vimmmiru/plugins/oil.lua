42799748003dd579b7e75189fdf26f36d92f7133
iammmiru <32926337+iammmiru@users.noreply.github.com>
2025-05-25 16:00:31 +0200

    feat(oil): add oil.nvim plugin with custom config and keymap
    - Add `stevearc/oil.nvim` plugin with custom columns and hidden file logic
    - Add dependency on `mini.icons`
    - Set up `<leader>ex` keymap to open Oil
    - Update `lazy-lock.json` for new plugins
    - Remove old `<leader>ex` keymap from `remap.lua`

---
+++ lua/vimmmiru/plugins/oil.lua
@@ -0,0 +1,32 @@
+---@diagnostic disable: undefined-doc-name
+return {
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		columns = {
+			"icon",
+			-- "permissions",
+			"size",
+			"mtime",
+		},
+		view_options = {
+			show_hidden = true,
+			is_always_hidden = function (name, _)
+				-- hide .DS_Store
+				local m = name:match("^%.DS_Store$")
+				return m ~= nil
+			end
+		}
+	},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	config = function(_, opts)
+		require("oil").setup(opts)
+		-- -- Keymaps
+		vim.keymap.set("n", "<leader>ex", "<cmd>Oil<cr>", { desc = "Open Oil" })
+	end,
+}
