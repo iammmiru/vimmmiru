00a3172e2506b9c84eeef5eb1a01013d54854095
iammmiru <32926337+iammmiru@users.noreply.github.com>
2025-05-27 13:34:36 +0200

    feat(oil): add custom keymaps and change open mapping to '-'
    - Added custom keymaps for Oil: disabled '`' and mapped 'cd' to actions.cd in normal mode.
    - Changed the keybinding to open Oil from <leader>ex to '-'.

--- lua/vimmmiru/plugins/oil.lua
+++ lua/vimmmiru/plugins/oil.lua
@@ -12,11 +12,15 @@ return {
 		},
 		view_options = {
 			show_hidden = true,
-			is_always_hidden = function (name, _)
+			is_always_hidden = function(name, _)
 				-- hide .DS_Store
 				local m = name:match("^%.DS_Store$")
 				return m ~= nil
 			end
+		},
+		keymaps = {
+			["`"] = false,
+			["cd"] = { "actions.cd", mode = "n" }
 		}
 	},
 	-- Optional dependencies
@@ -27,6 +31,6 @@ return {
 	config = function(_, opts)
 		require("oil").setup(opts)
 		-- -- Keymaps
-		vim.keymap.set("n", "<leader>ex", "<cmd>Oil<cr>", { desc = "Open Oil" })
+		vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open Oil" })
 	end,
 }
