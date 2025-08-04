23bc1b2f470b29ec67f39c944669b6e6cb362201
iammmiru <32926337+iammmiru@users.noreply.github.com>
2025-08-04 10:38:17 +0200

    feat(java): enhance jdtls setup with dynamic paths and workspace
    - Add OS detection for jdtls config directory
    - Set explicit paths for jdtls jar, lombok, and workspace directory
    - Use custom root_dir detection with jdtls.setup
    - Switch to explicit Java binary in cmd

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,5 +1,28 @@
+local SYSTEM = "linux"
+if vim.fn.has("mac") == 1 then
+  SYSTEM = "mac"
+end
+
+local jdtls_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+local path_to_lsp_server = jdtls_path .. "/config_" .. SYSTEM
+local path_to_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+local lombok_path = jdtls_path .. "/lombok.jar"
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root/" .. project_name
+
 local config = {
-  cmd = { "jdtls" },
-  root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+  cmd = {
+    "/opt/homebrew/bin/java",
+    "-javaagent:" .. lombok_path,
+    "-jar",
+    path_to_jar,
+    "-configuration",
+    path_to_lsp_server,
+    "-data",
+    workspace_dir,
+  },
+
+  root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "build.gradle" }),
 }
 require("jdtls").start_or_attach(config)
