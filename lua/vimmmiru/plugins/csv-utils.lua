5db1ce23b17b19518d095365e596e85c0cb242a7
iammmiru <32926337+iammmiru@users.noreply.github.com>
2025-08-21 09:43:00 +0200

    chore(unixtime-utils): rename csv-utils -> unixtime-utils

--- lua/vimmmiru/plugins/csv-utils.lua
+++
@@ -1,5 +0,0 @@
-return {
-  dir = "~/my_project/nvim_plugins/csv-utils.nvim",
-  dev = true,
-  lazy = false,
-}
---
+++ lua/vimmmiru/plugins/unixtime-utils.lua
@@ -0,0 +1,5 @@
+return {
+  dir = "~/my_project/nvim_plugins/unixtime-utils.nvim",
+  dev = true,
+  lazy = false,
+}
