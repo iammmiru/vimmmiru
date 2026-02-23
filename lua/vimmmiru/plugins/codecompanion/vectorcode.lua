ef75014440037c6c80054441c8922a00471b3463
iammmiru <32926337+iammmiru@users.noreply.github.com>
2026-02-23 09:55:41 +0100

    clean up unused plugins

--- lua/vimmmiru/plugins/codecompanion/vectorcode.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Davidyz/VectorCode",
-  version = "*", -- optional, depending on whether you're on nightly or release
-  build = "uv tool upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {
-    async_backend = "lsp",
-  },
-}
