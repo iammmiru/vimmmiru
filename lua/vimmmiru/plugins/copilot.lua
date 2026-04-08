49c81133f768c7ea23ec97386c07ab07bba41b90
iammmiru <32926337+iammmiru@users.noreply.github.com>
2026-04-08 14:26:36 +0200

    copilot: add neetcode to exclude dirs

--- lua/vimmmiru/plugins/copilot.lua
+++ lua/vimmmiru/plugins/copilot.lua
@@ -9,9 +9,9 @@ return {
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
-    local exclude_dirs = { vim.fn.expand("$HOME/aivis/engine-v2") }
+    local exclude_dirs = { vim.fn.expand("$HOME/aivis/engine-v2"), vim.fn.expand("$HOME/my_project/neetcode") }
     require("copilot").setup({
-      should_attach = function(_, bufname)
+      should_attach = function(_, _)
         local root_dir = require("lspconfig.util").root_pattern(".git")(vim.fn.expand("%:p"))
         for _, dir in ipairs(exclude_dirs) do
           if dir == root_dir then
